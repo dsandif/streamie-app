c6e495ba5aeaa03d5884442b85992f634ba509e8
Darien Sandifer <dariens@duck.com>
2023-02-10 00:12:56 -0600

    updates to video-streaming microservice

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,35 +1,66 @@
 const express = require('express')
-const fs = require("fs")
+const http = require('http')
 
 const app = express()
 
 if (!process.env.PORT) {
-  throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.")
 }
-const port = process.env.PORT
+
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.")
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.")
+}
+
+const {
+  PORT,
+  VIDEO_STORAGE_HOST } = process.env
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT)
 
 app.get('/',(req, res)=>{
   res.send("Streamie app")
 })
 
 app.get('/video',(req, res)=>{
-  //stats retrieves the video file size
-  fs.stat(path="filepath",(err, stats)=>{
-    if(err){
-      console.error("An error occurred fetching the video.")
-      res.sendStatus(500)
-      return
+  console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`)
+  const videoReqData = {
+    host: VIDEO_STORAGE_HOST,
+    port: VIDEO_STORAGE_PORT,
+    path: '/video?path=bmw_love.mp4', // Video path is hard-coded for the moment.
+    method: 'GET',
+    headers: req.headers
+  }
+  const forwardRequest = http.request( // Forward the request to the video storage microservice.
+    videoReqData,
+    forwardResponse => {
+        res.writeHeader(forwardResponse.statusCode, forwardResponse.headers)
+        forwardResponse.pipe(res)
     }
+  )
 
-    //send response header to the browser
-    res.writeHead(200, {
-      "Content-Length" : stats.size,
-      "Content-Type": "video/mp4"
-    })
+  req.pipe(forwardRequest)
 
-    //stream video to the browser
-    fs.createReadStream(path).pipe(res)
-  })
+  // original file system location
+  // //stats retrieves the video file size
+  // fs.stat(path="filepath",(err, stats)=>{
+  //   if(err){
+  //     console.error("An error occurred fetching the video.")
+  //     res.sendStatus(500)
+  //     return
+  //   }
+
+  //   //send response header to the browser
+  //   res.writeHead(200, {
+  //     "Content-Length" : stats.size,
+  //     "Content-Type": "video/mp4"
+  //   })
+
+  //   //stream video to the browser
+  //   fs.createReadStream(path).pipe(res)
+  // })
 })
 
 app.listen(port, () =>{
