8074c995696bc7720738caf9d496158fae001bdc
Darien Sandifer <dariens@duck.com>
2023-02-18 23:43:49 -0600

    added mongo database

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const http = require('http')
+const mongodb = require("mongodb");
 
 const app = express()
 
@@ -17,52 +18,68 @@ if (!process.env.VIDEO_STORAGE_PORT) {
 
 const {
   PORT,
-  VIDEO_STORAGE_HOST } = process.env
+  VIDEO_STORAGE_HOST,
+  DBHOST,
+  DBNAME,
+ } = process.env
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT)
 
-app.get('/',(req, res)=>{
-  res.send("Streamie app")
-})
+function main(){
+  return mongodb.MongoClient.connect(DBHOST)
+    .then(client=>{
+      const db = client.db(DBNAME)
+      const videosCollection = db.collection("videos")
 
-app.get('/video',(req, res)=>{
-  console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`)
-  const videoReqData = {
-    host: VIDEO_STORAGE_HOST,
-    port: VIDEO_STORAGE_PORT,
-    path: '/video?path=next_video.mp4', // Video path is hard-coded for the moment. Should show color-bars.
-    method: 'GET',
-    headers: req.headers
-  }
-  const forwardRequest = http.request( // Forward the request to the video storage microservice.
-    videoReqData,
-    forwardResponse => {
-        res.writeHeader(forwardResponse.statusCode, forwardResponse.headers)
-        forwardResponse.pipe(res)
-    }
-  )
+      app.get('/video',(req, res)=>{
+        console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`)
+        const videoId = new mongodb.ObjectId(req.query.id)
+        
+        videosCollection
+          .findOne({_id: videoId})
+            .then(videoRecord =>{
+              if(!videoRecord){
+                res.sendStatus(404)
+                return
+              }
+            
+            const videoReqData = {
+              host: VIDEO_STORAGE_HOST,
+              port: VIDEO_STORAGE_PORT,
+              path: `/video?path=${videoRecord.videoPath}`, // Video path is hard-coded for the moment. Should show color-bars.
+              method: 'GET',
+              headers: req.headers
+            }
 
-  req.pipe(forwardRequest)
+            const forwardRequest = http.request( // Forward the request to the video storage microservice.
+              videoReqData,
+              forwardResponse => {
+                  res.writeHeader(forwardResponse.statusCode, forwardResponse.headers)
+                  forwardResponse.pipe(res)
+              }
+            )
+          
+            req.pipe(forwardRequest)
+          })
+          .catch(err=>{
+            console.error("Database query failed")
+            console.error(err && err.stack || err);
+            res.sendStatus(500);
+          })
+      })
 
-  // original file system location
-  // //stats retrieves the video file size
-  // fs.stat(path="filepath",(err, stats)=>{
-  //   if(err){
-  //     console.error("An error occurred fetching the video.")
-  //     res.sendStatus(500)
-  //     return
-  //   }
+      app.get('/',(req, res)=>{
+        res.send("Streamie app")
+      })
+      
+      app.listen(PORT, () =>{
+        console.log(`microservice online 🚀🚀🚀`)
+      })
+  })
+}
 
-  //   //send response header to the browser
-  //   res.writeHead(200, {
-  //     "Content-Length" : stats.size,
-  //     "Content-Type": "video/mp4"
-  //   })
-
-  //   //stream video to the browser
-  //   fs.createReadStream(path).pipe(res)
-  // })
-})
-
-app.listen(port, () =>{
-  console.log(`video streaming microservice up and listening on port ${port} 🚀🚀🚀`)
+main()
+.then(()=> console.log(`video streaming microservice up and listening on port ${PORT} 🚀🚀🚀`))
+.catch(err=>{
+  console.error("Microservice failed to start.")
+  console.error(err && err.stack || err)
 })
